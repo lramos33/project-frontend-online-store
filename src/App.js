f4c3650615385c53e78a2410c0a2649c785303c6
arturbacilla <artur.bacilla1@gmail.com>
2021-10-25 17:30:30 -0300

    Req 5: Adicionado botão, feito handleChange e parcial fetchAPI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 import { ProductList } from './pages/ProductList';
 import { ShoppingCart } from './pages/ShoppingCart';
+
 export class App extends Component {
   render() {
     return (
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
 export class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  fetchProductAPI = async () => {
+    const category = 'MLB271599';
+    const { searchInput } = this.state;
+    const fetchedProducts = await getProductsFromCategoryAndQuery(category, searchInput);
+    console.log(fetchedProducts);
+  }
+
+  handleChange = (whichInput) => {
+    const { value } = whichInput.target;
+    if (value || value === '') {
+      this.setState({ searchInput: value });
+    }
+  }
+
   render() {
     return (
       <div>
-        <input type="text" name="sometext" />
-        <Link data-testid="shopping-cart-button" to="/shopping-cart">Carrinho</Link>
-        <h3 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h3>
+        <header>
+          <input
+            data-testid="query-input"
+            type="text"
+            name="sometext"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="query-button"
+            type="submit"
+            onClick={ this.fetchProductAPI }
+          >
+            Procurar
+          </button>
+          <h3 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h3>
+          <Link data-testid="shopping-cart-button" to="/shopping-cart">Carrinho</Link>
+        </header>
+        <section>
+          <span>Conteudo</span>
+        </section>
       </div>
     );
   }
